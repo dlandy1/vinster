fc32597796fbca2aa097bc66d2d42eafbc6f7166
Daniel Landy <dlandy1@babson.edu>
2014-03-29 17:34:54 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vinster::Application.config.secret_key_base = '9da8d6ffa70ae5c4ca271fb87d7eb04675f4a51655faf44f3f877a5956f5dafb71caa7e3233866c49985243ff1d27eb56db062123a625dceacd86967e5e4d7ec'
